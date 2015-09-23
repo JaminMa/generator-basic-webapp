f3937ea7250324236788ea04feeed7c45b6d3608
Jamin Ma <raspi.jm@gmail.com>
2015-09-23 13:45:18 -0400

    Utilize process.env.PORT for Express
    The scaffolded Express server should use the PORT environment variable
    if it exists. Otherwise, it will default to PORT 3000.

--- app/templates/_server.js
+++ app/templates/_server.js
@@ -6,8 +6,9 @@ var app = express();
 // Where to serve static content
 app.use(express.static(__dirname + '/dist'));
 
-// Start server on port 3000
-var port = 3000;
+// Start listening to port
+var port = process.env.PORT || 3000;
 app.listen(port, function(){
     console.log('Express server listening on port %d in %s mode', port, app.settings.env);
 });
+
